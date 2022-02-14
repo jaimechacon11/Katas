ebd4a1fbc3fb746eb2693e46aac97b92d3eafa51
jaimechacon11 <chacon.jaime99g@gmail.com>
2022-02-14 01:35:21 -0600

    Fin Katas Completas

---
+++ Kata_Módulo_10/config.py
@@ -0,0 +1,19 @@
+# def main():
+#     try:
+#         configuration = open("config.txt")
+#     except Exception:
+#         print("Couldn't find the config.txt file!")
+
+def main():
+    try:
+        configuration = open('prueba')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except (IsADirectoryError,PermissionError):
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+
+
+if __name__ == '__main__':
+    main()
